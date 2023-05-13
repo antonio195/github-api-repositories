baa9497e8d0758d3ce6aaab9ca59cebc81df6d94
antonio195 <antonio195br@gmail.com>
2023-05-12 22:39:19 -0300

    Todas requisicoes ok

--- app/src/main/java/com/antoniocostadossantos/githubapirepositories/util/ExtensionFuncions.kt
+++ app/src/main/java/com/antoniocostadossantos/githubapirepositories/util/ExtensionFuncions.kt
@@ -1,14 +1,18 @@
 package com.antoniocostadossantos.githubapirepositories.util
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE
 import com.antoniocostadossantos.githubapirepositories.R
+import com.antoniocostadossantos.githubapirepositories.ui.WebViewActivity
 
 fun AppCompatActivity.startFragment(fragment: Fragment, bundle: Bundle? = null): Boolean {
     fragment.arguments = bundle
     val transaction = supportFragmentManager.beginTransaction()
     transaction.replace(R.id.main_fragment_container_view, fragment)
+    transaction.setTransition(TRANSIT_FRAGMENT_FADE)
     transaction.addToBackStack(null)
     transaction.commit()
     return true
@@ -18,7 +22,15 @@ fun Fragment.startFragment(fragment: Fragment, bundle: Bundle? = null): Boolean 
     fragment.arguments = bundle
     val transaction = activity?.supportFragmentManager?.beginTransaction()!!
     transaction.replace(R.id.main_fragment_container_view, fragment)
+    transaction.setTransition(TRANSIT_FRAGMENT_FADE)
     transaction.addToBackStack(null)
     transaction.commit()
     return true
+}
+
+fun Fragment.startLink(url: String) {
+    Intent(requireContext(), WebViewActivity::class.java).apply {
+        putExtra("URL", url)
+        startActivity(this)
+    }
 }
